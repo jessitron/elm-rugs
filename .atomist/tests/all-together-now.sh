e05de34d29fe609e4c0a00cac1777710f8e0a751
Jessica Kerr <jessitron@gmail.com>
2017-03-06 19:12:35 -0600

    Clear everything out except the generator

--- .atomist/tests/all-together-now.sh
+++ .atomist/tests/all-together-now.sh
@@ -13,12 +13,12 @@ rug -lRC $project_parent_dir generate StaticPage $project_name
 
 project_dir=$project_parent_dir/$project_name
 
-rug -lRC $project_dir edit UpgradeToBeginnerProgram
-rug -lRC $project_dir edit AddButton button_text="Hello There" button_message=HelloThere
-rug -lRC $project_dir edit AddTextInput input_name=beginnerInput
-rug -lRC $project_dir edit UpgradeToProgram
-rug -lRC $project_dir edit AddButton button_text="Hello Again" button_message=HelloAgain
-rug -lRC $project_dir edit AddTextInput input_name=advancedInput
+#rug -lRC $project_dir edit UpgradeToBeginnerProgram
+#rug -lRC $project_dir edit AddButton button_text="Hello There" button_message=HelloThere
+#rug -lRC $project_dir edit AddTextInput input_name=beginnerInput
+#rug -lRC $project_dir edit UpgradeToProgram
+#rug -lRC $project_dir edit AddButton button_text="Hello Again" button_message=HelloAgain
+#rug -lRC $project_dir edit AddTextInput input_name=advancedInput
 rug -lRC $project_dir edit SubscribeToClicks
 
 cd $project_dir
