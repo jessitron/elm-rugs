2b46a0cc7e263a0419756e163531759451a12558
Atomist <cli@atomist.com>
2017-06-02 17:54:01 -0500

    Adds a TypeScript Rug editor to a Rug project
    ```
    ---
    kind: "operation"
    client: "com.atomist:rug"
    version: "1.0.1-LANGEXT"
    editor:
      name: "AddTypeScriptEditor"
      group: "atomist"
      artifact: "rug-rugs"
      version: "0.31.0-20170601202105"
      origin:
        repo: "https://github.com/atomist/rug-rugs.git"
        branch: "d1145f3450f22af598a278ccc475eb18f3c0f5e4"
        sha: "d1145f3"
      parameters:
        - "editorName": "AddFunction"
        - "description": "add a function to an Elm program"
    
    ```

---
+++ .atomist/editors/AddFunction.ts
@@ -0,0 +1,30 @@
+import { File, Project } from "@atomist/rug/model/Core";
+import { Editor, Parameter, Tags } from "@atomist/rug/operations/Decorators";
+import { EditProject } from "@atomist/rug/operations/ProjectEditor";
+import { Pattern } from "@atomist/rug/operations/RugOperation";
+
+/**
+ * Sample TypeScript editor used by AddAddFunction.
+ */
+@Editor("AddFunction", "add a function to an Elm program")
+@Tags("documentation")
+export class AddFunction implements EditProject {
+
+    @Parameter({
+        displayName: "Some Input",
+        description: "example of how to specify a parameter using decorators",
+        pattern: Pattern.any,
+        validInput: "a description of the valid input",
+        minLength: 1,
+        maxLength: 100,
+    })
+    public inputParameter: string;
+
+    public edit(project: Project) {
+        const certainFile = project.findFile("hello.txt");
+        const newContent = certainFile.content.replace(/the world/, this.inputParameter);
+        certainFile.setContent(newContent);
+    }
+}
+
+export const addFunction = new AddFunction();
